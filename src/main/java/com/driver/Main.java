a5d5034d730fb8863f268f91f1fbb7c0606d6c8f
Mustafa Hussain <118181312+MustafaHussain52@users.noreply.github.com>
2023-02-12 18:51:46 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+
 public class Main {
-  
+
+    public static void main(String args[]){
+        RWOnly obj=new RWOnly();
+        //obj.x=1;
+        //System.out.println(obj.x);
+        obj.setName("mustafa");
+        obj.getName();
+    }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+
 public class RWOnly {
-    
+    private String name;
+    public void setName(String n){
+        name=n;
+    }
+    public String getName(){
+        return name;
+    }
+
 }
